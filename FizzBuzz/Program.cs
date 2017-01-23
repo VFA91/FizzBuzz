c8cb7ae28c9ef0d28dc19efc8035be560dcf67bb
vfernandezantolin <vfernandezantolin@ESWSZ3T6EDJL>
2017-01-23 14:55:38 +0100

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        private const int NUMBEROFNUMBERS = 100;
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Start();
+        }
+
+        private void Start()
+        {
+            Console.WriteLine("Programa FizzBuzz");
+
+            StringBuilder result = new StringBuilder();
+            result = PrintNumbers(result);
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        private StringBuilder PrintNumbers(StringBuilder result)
+        {
+            for (int i = 1; i <= NUMBEROFNUMBERS; i++)
+            {
+                result.Append(GetNumberOrString(i) + ",");
+            }
+
+            FormatResult(result);
+            return result;
+        }
+
+        private void FormatResult(StringBuilder result)
+        {
+            result.Remove(result.Length - 1, 1);
+        }
+
+        private string GetNumberOrString(int number)
+        {
+            if (IsMultipleOfFiveAndThree(number))
+                return "Fizz Buzz";
+            else if (IsMultipleOfFive(number))
+                return "Buzz";
+            else if (IsMultipleOfThree(number))
+                return "Fizz";
+            else
+                return number.ToString();
+        }
+
+        private bool IsMultipleOfThree(int number)
+        {
+            return number % 3 == 0;
+        }
+
+        private bool IsMultipleOfFive(int number)
+        {
+            return number % 5 == 0;
+        }
+
+        private bool IsMultipleOfFiveAndThree(int number)
+        {
+            return number % 3 == 0 && number % 5 == 0;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,2 @@
+Escriba un programa que imprima los números del 1 al 100. Pero para los múltiplos de 3 imprima "Fizz" 
+en lugar del número y para los múltiplos de 5 imprima "Buzz". Para los números que son múltiplos de ambos imprima "FizzBuzz"
